3b238bc8994eb58c977bf652be17531d910e041d
amarriner <amarriner@amarriner.com>
2011-08-22 13:00:09 -0400

    Initial commit, skeleton

---
+++ House/House.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using Terraria_Server;
+using Terraria_Server.Plugin;
+using Terraria_Server.Collections;
+using Terraria_Server.Commands;
+using Terraria_Server.Events;
+using Terraria_Server.Logging;
+
+namespace House
+{
+    public class House : Plugin
+    {
+        public static House plugin;
+
+        public override void Load()
+        {
+            Name = "House";
+            Description = "A plugin to allow players to define a safe area";
+            Author = "amarriner";
+            Version = "0.1";
+            TDSMBuild = 31;
+
+            plugin = this;
+        }
+
+        public override void Enable()
+        {
+            Program.tConsole.WriteLine(base.Name + " enabled.");
+        }
+
+        public override void Disable()
+        {
+            Program.tConsole.WriteLine(base.Name + " disabled.");
+        }
+    }
+}
